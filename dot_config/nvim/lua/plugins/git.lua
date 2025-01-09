0325d9687880bd17d822ee895717b4c56860d938
Tim Eichholz <t.eichholz@reply.de>
2025-01-09 21:01:04 +0100

    more

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -29,16 +29,16 @@ return {
       signs = {
         add = { text = "▎" },
         change = { text = "▎" },
-        delete = { text = "" },
-        topdelete = { text = "" },
+        delete = { text = "▎" },
+        topdelete = { text = "▎" },
         changedelete = { text = "▎" },
         untracked = { text = "▎" },
       },
       signs_staged = {
         add = { text = "▎" },
         change = { text = "▎" },
-        delete = { text = "" },
-        topdelete = { text = "" },
+        delete = { text = "▎" },
+        topdelete = { text = "▎" },
         changedelete = { text = "▎" },
       },
       on_attach = function(buffer)
