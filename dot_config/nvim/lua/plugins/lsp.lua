fa55e4b0949ad0fc909f452b8eddb453a2cc492f
Tim Eichholz <tim.eichholz98@web.de>
2025-03-24 20:18:11 +0100

    schemastore

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -12,6 +12,34 @@ return {
           require("lspconfig").bashls.setup {
             filetypes = { "bash", "zsh", "sh" },
           }
+        end,
+
+        ["jsonls"] = function()
+          require('lspconfig').jsonls.setup {
+            settings = {
+              json = {
+                schemas = require('schemastore').json.schemas(),
+                validate = { enable = true },
+              },
+            },
+          }
+        end,
+
+        ["yamlls"] = function()
+          require('lspconfig').yamlls.setup {
+            settings = {
+              yaml = {
+                schemaStore = {
+                  -- You must disable built-in schemaStore support if you want to use
+                  -- this plugin and its advanced options like `ignore`.
+                  enable = false,
+                  -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                  url = "",
+                },
+                schemas = require('schemastore').yaml.schemas(),
+              },
+            },
+          }
         end
       }
 
@@ -30,6 +58,9 @@ return {
       { "<leader>cr", vim.lsp.buf.rename, desc = "Rename" },
       -- { "<leader>cf", vim.lsp.buf.format, desc = "Format" },
     },
+  },
+  {
+    "b0o/schemastore.nvim",
   },
   {
     'stevearc/conform.nvim',
